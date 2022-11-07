9ad1563f42d407464f86cecd95f1dc2b45f7193d
solomon244 <slegesse2444@gmail.com>
2022-11-07 03:36:05 +0300

    Strace is your friend

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,7 @@
+# Using strace, find out why Apache
+# is returning a 500 error.
+
+exec { 'fix-wordpress':
+  command => 'sed -i "s/class-wp-locale.phpp/class-wp-locale.php/g" /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/',
+}
